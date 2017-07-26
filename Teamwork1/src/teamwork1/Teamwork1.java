b0534efa0bd773a5de1bb2b2e45b8fa3c61fafc8
acer <acer@acer-pc.lan>
2017-07-26 23:35:57 +0200

    zkouším push

--- Teamwork1/src/teamwork1/Teamwork1.java
+++ Teamwork1/src/teamwork1/Teamwork1.java
@@ -15,7 +15,7 @@ public class Teamwork1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //David - domácí pokus o push do repo Teamwork
     }
     
 }
